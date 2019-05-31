ba7b3758229ed948677719ad3a33188da234f7ee
Michał Martyniuk <michal.martyniuk7@gmail.com>
2019-05-31 11:12:53 +0200

    gulpfile insertScripts added

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,15 +4,21 @@ import concat from 'gulp-concat'
 import uglify from 'gulp-uglify'
 import nodemon from 'gulp-nodemon'
 import { exec } from 'child_process'
-import fs from 'fs'
+import fs from 'fs';
+import { getPartOfString, insertContent } from '../src/funcs';
 
 function watcher() {
     watch('src/*\.js', js)
 }
 function insertScripts() {
-    let webpack = `"webpack": "webpack"`
-    let watch = `"webpack-watch": "webpack --watch"`
-    fs.readdirSync('./')
+    let newScripts = `"scripts": {
+        "webpack": "webpack",
+        "webpack-watch: "webpack --watch"
+      `
+    let packageJson = fs.readFileSync('package.json').toString()
+    let { start, end } = getPartOfString(packageJson, `"scripts": {`, `}`)
+    let updatedContent = insertContent(packageJson, start, end, newScripts)
+    fs.writeFileSync("package.json", updatedContent)
 
 }
 function initNodemon(done) {
