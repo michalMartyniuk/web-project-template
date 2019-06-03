7c51dedc95d0e5b6cbe1152322d1344d40407611
Michał Martyniuk <michal.martyniuk7@gmail.com>
2019-06-03 11:01:15 +0200

    initial commit

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,30 +1,16 @@
-import { src, dest, watch, series, parallel } from 'gulp'
-import sass from 'gulp-sass'
-import concat from 'gulp-concat'
-import uglify from 'gulp-uglify'
+import { watch, series } from 'gulp'
 import nodemon from 'gulp-nodemon'
 import { exec } from 'child_process'
-import fs from 'fs';
-import { getPartOfString, insertContent } from '../src/funcs';
 
 function watcher() {
     watch('src/*\.js', js)
 }
-function insertScripts() {
-    let newScripts = `"scripts": {
-        "webpack": "webpack",
-        "webpack-watch: "webpack --watch"
-      `
-    let packageJson = fs.readFileSync('package.json').toString()
-    let { start, end } = getPartOfString(packageJson, `"scripts": {`, `}`)
-    let updatedContent = insertContent(packageJson, start, end, newScripts)
-    fs.writeFileSync("package.json", updatedContent)
 
-}
 function initNodemon(done) {
     nodemon({ script: "build/build.js" })
     done()
 }
+
 function webpackWatch(done) {
     exec('npm run webpack-watch', (err, output) => {
         if (err) console.log(err)
@@ -32,4 +18,5 @@ function webpackWatch(done) {
     })
     done()
 }
+
 export default series(webpackWatch, initNodemon)
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,36 @@
 const path = require("path");
+const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 const webpack = require("webpack");
-// const HtmlWebpackPlugin = require("html-webpack-plugin");
-// const BrowserSyncPlugin = require("browser-sync-webpack-plugin");
 
 module.exports = {
-  // plugins: [
-  //   new BrowserSyncPlugin({
-  //     host: "localhost",
-  //     port: 7777,
-  //     server: "./",
-  //     files: "./index.html"
-  //   }),
+  plugins: [
+    new BrowserSyncPlugin({
+      host: "localhost",
+      port: 7777,
+      server: "./",
+      files: "./index.html"
+    }),
 
-  //   new webpack.ProvidePlugin({
-  //     $: "jquery",
-  //     jQuery: "jquery"
-  //   })
-  // ],
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ],
 
   mode: "development",
   target: "node",
   entry: "./src/app.js",
   output: {
     path: path.resolve(__dirname, "build"),
-    filename: "build.js"
-  },
+    filename: "build.js" },
 
   module: {
     rules: [
-      {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      },
-      {
-        test: /\.js$/,
-        use: {
+      { test: /\.scss$/, 
+        use: ["style-loader", "css-loader", "sass-loader"] },
+        
+      { test: /\.js$/,
+        use: { 
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"]
-          }
-        }
-      }
-    ]
-  }
-};
+          options: { presets: ["@babel/preset-env"] }}}]}};
 
